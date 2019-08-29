5f9f6068d1e50851cfdbe46d580cbe25f71efd9b
Guzman Monne <guzmonne@hotmail.com>
2019-08-29 12:36:49 -0300

    finished first version of the BaseInterfaces class

--- app/utils/base_interfaces.py
+++ app/utils/base_interfaces.py
@@ -3,11 +3,28 @@ from flask_restplus import fields as restplus_fields
 
 class BaseInterfaces(object):
     """ Base interfaces class """
+    update_keys = []
 
-    def __init__(self):
+    def __init__(self, api, name):
+        self._api = api
+        self._name = name
         self._schema = self.create_marshmallow_schema()
         self.single_schema = self._schema()
         self.many_schema = self._schema(many=True)
+        self.create_model_keys = getattr(self, 'create_model_keys', None) or self.get_restplus_fields_keys()
+        self.create_model = self._api.model(self._name + 'Create', self.get_restplus_fields(self.create_model_keys))
+        self.update_model_keys = getattr(self, 'update_model_keys', None) or self.create_model_keys
+        self.update_model = self._api.model(self._name + 'Update', self.get_restplus_fields(self.update_model_keys))
+        self.model = self._api.model(self._name, self.get_restplus_fields(self.get_restplus_fields_keys()))
+
+    def get_restplus_fields(self, keys):
+        return {key: getattr(self, key).get('r') for key in self.get_restplus_fields_keys() if key in keys}
+
+    def get_restplus_fields_keys(self):
+        return [field for field in self.attributes() if
+            type(getattr(self, field)) is dict
+            and getattr(self, field).get('r') is not None
+        ]
 
     def get_marshmallow_fields_keys(self):
         return [field for field in self.attributes() if
--- app/utils/base_interfaces_test.py
+++ app/utils/base_interfaces_test.py
@@ -1,21 +1,38 @@
 import inspect
 import pytest
-from marshmallow import exceptions
+from flask_restplus import Namespace
 
-from .base_interfaces import BaseInterfaces, marshmallow_fields
+from .base_interfaces import BaseInterfaces, marshmallow_fields, restplus_fields
 
 class ChildInterfaces(BaseInterfaces):
-    id = dict(m=marshmallow_fields.Int(attribute='id'))
-    first_name = dict(m=marshmallow_fields.String(attribute='firstName'))
-    admin = dict(m=marshmallow_fields.Bool(attribute='admin'))
+    id = dict(
+        m=marshmallow_fields.Int(attribute='id'),
+        r=restplus_fields.Integer(description='Unique identifier', required=True, example=123),
+    )
+    first_name = dict(
+        m=marshmallow_fields.String(attribute='firstName'),
+    )
+    firstName = dict(
+        r=restplus_fields.String(description='User first name', required=False, example="Example"),
+    )
+    admin = dict(
+        m=marshmallow_fields.Bool(attribute='admin', default=False, required=True),
+        r=restplus_fields.Boolean(description='User is Admin?', required=False, example=False, default=False)
+    )
+    create_model_keys = ['firstName', 'admin']
+    update_model_keys = ['admin']
 
 @pytest.fixture
-def base():
-    return BaseInterfaces()
+def api():
+    return Namespace('Entity', description="Entity resources")
 
 @pytest.fixture
-def child():
-    return ChildInterfaces()
+def base(api):
+    return BaseInterfaces(api, 'Entity')
+
+@pytest.fixture
+def child(api):
+    return ChildInterfaces(api, 'Entity')
 
 def test_class_existance(base):
     assert base != None
@@ -36,24 +53,36 @@ def test_create_marshmallow_schema_result(child):
 def test_schema_is_valid(child):
     schema = child._schema()
     actual = schema.dump(dict(id='not an int')).data
-    assert actual == dict()
+    assert actual == dict(admin=False)
     actual = schema.dump(dict(id=1)).data
-    assert actual == dict(id=1)
-    actual = schema.dump(dict(id=1, firstName='Example', admin=False)).data
-    assert actual == dict(id=1, first_name='Example', admin=False)
+    assert actual == dict(id=1, admin=False)
+    actual = schema.dump(dict(id=1, firstName='Example', admin=True)).data
+    assert actual == dict(id=1, first_name='Example', admin=True)
 
 def test_single_schema_works(child):
     actual = child.single_schema.dump(dict(id='not an int')).data
-    assert actual == dict()
+    assert actual == dict(admin=False)
     actual = child.single_schema.dump(dict(id=1)).data
-    assert actual == dict(id=1)
-    actual = child.single_schema.dump(dict(id=1, firstName='Example', admin=False)).data
-    assert actual == dict(id=1, first_name='Example', admin=False)
+    assert actual == dict(id=1, admin=False)
+    actual = child.single_schema.dump(dict(id=1, firstName='Example', admin=True)).data
+    assert actual == dict(id=1, first_name='Example', admin=True)
 
 def test_many_schema_works(child):
     actual = child.many_schema.dump([dict(id='not an int')]).data
-    assert actual == [dict()]
+    assert actual == [dict(admin=False)]
     actual = child.many_schema.dump([dict(id=1)]).data
-    assert actual == [dict(id=1)]
-    actual = child.many_schema.dump([dict(id=1, firstName='Example', admin=False)]).data
-    assert actual == [dict(id=1, first_name='Example', admin=False)]
+    assert actual == [dict(id=1, admin=False)]
+    actual = child.many_schema.dump([dict(id=1, firstName='Example', admin=True)]).data
+    assert actual == [dict(id=1, first_name='Example', admin=True)]
+
+def test_create_model_is_valid(child):
+    print(child.create_model)
+    assert str(child.create_model) == 'Model(EntityCreate,{admin,firstName})'
+
+def test_update_model_is_valid(child):
+    print(child.update_model)
+    assert str(child.update_model) == 'Model(EntityUpdate,{admin})'
+
+def test_model_is_valid(child):
+    print(child.model)
+    assert str(child.model) == 'Model(Entity,{admin,firstName,id})'
